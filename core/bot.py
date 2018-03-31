68c2d30615dce8d6bfaa59b451d06ac68a928344
Riley Winkler <rex8112@gmail.com>
2018-03-31 04:56:33 -0500

    Added a few basic commands
    Includes a User Count, Shutdown, and a not working username change command.

--- core/bot.py
+++ core/bot.py
@@ -19,6 +19,24 @@ async def ping(ctx):
 async def joined(ctx, member : discord.Member):
 	"""Says when a user joined"""
 	await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+	
+@bot.command()
+async def users(ctx):
+	"""Lists Users Count"""
+	usrs = len(bot.users)
+	await ctx.send('{0} mortals exist in this Dimension'.format(usrs))
+	
+@bot.command()
+async def shutdown(ctx):
+	"""Turns the bot off"""
+	if bot.is_owner(discord.Message.author):
+		await ctx.send('Goodbye...')
+		await bot.logout()
+		sys.exit()
+
+@bot.command()
+async def name(ctx, new):
+	await discord.ClientUser.edit(username=new)
 
 	
 	
