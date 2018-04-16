c24e8d13f555e58fb22e734bf94e573afba88b01
Riley Winkler <rex8112@gmail.com>
2018-04-16 17:03:55 -0500

    Added setXP and retrieveXP

--- cogs/xp.py
+++ cogs/xp.py
@@ -1,11 +1,27 @@
 import discord
 from discord.ext import commands
+from cogs.tools.database import *
 
 
 class XP:
 	def __init__(self, bot):
 		self.bot = bot
+		initDB()
 	
+	@commands.command()
+	@commands.guild_only()
+	async def setXP(self, ctx, mem: discord.Member, amt: int):
+		if getXP(mem):
+			updateXP(mem, amt)
+		elif getXP(mem) is None:
+			addMem(mem)
+			updateXP(mem, amt)
+	
+	@commands.command()
+	@commands.guild_only()
+	async def retrieveXP(self, ctx, mem: discord.Member):
+		xp = getXP(mem)
+		await ctx.send('{} has {} XP'.format(mem.name, xp))
 	
 def setup(bot):
 	bot.add_cog(XP(bot))
