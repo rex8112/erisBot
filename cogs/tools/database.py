ad3464e6e9ce03cc219f85c8265d9748f4000c17
Riley Winkler <rex8112@gmail.com>
2018-04-16 17:03:21 -0500

    Fixed database.py
    Added commit to each function that needs it and checks if xp is NoneType

--- cogs/tools/database.py
+++ cogs/tools/database.py
@@ -1,25 +1,31 @@
 import sqlite3
 import discord
-db = sqlite3.connect('eris')
 
+db = sqlite3.connect('eris.db')
 cursor = db.cursor()
 
-def initDB():
-	cursor.execute( """CREATE TABLE IF NOT EXISTS members( index INTEGER PRIMARY KEY,
+def initDB():	
+	cursor.execute( """CREATE TABLE IF NOT EXISTS members( indx INTEGER PRIMARY KEY,
 		name TEXT, id INTEGER UNIQUE, totalXP INTEGER)""" )
+	db.commit()
 		
 def addMem(mem: discord.Member):
 	name = mem.name
 	id = mem.id
 	cursor.execute( """INSERT INTO members(name, id)
 										 VALUES(?, ?)""", (name, id))
+	db.commit()
 		
 def updateXP(mem: discord.Member, amt):
 	id = mem.id
 	cursor.execute( """UPDATE members SET totalXP = ? WHERE id = ?""", (amt, id))
+	db.commit()
 	
 def getXP(mem: discord.Member):
 	id = mem.id
-	cursor.execute( """SELECT totalXP FROM members WHERE id = ?""", (id))
+	cursor.execute( """SELECT totalXP FROM members WHERE id = ?""", (id,))
 	xp = cursor.fetchone()
-	return xp[0]
+	if xp:
+		return xp[0]
+	else:
+		return None
