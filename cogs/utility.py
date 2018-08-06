398277803f7b8dea3fa73869cd975b9cd2c7f652
Riley Winkler <rex8112@gmail.com>
2018-08-06 04:20:50 -0500

    Changed joined to whois

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,4 +1,5 @@
 import discord
+import datetime
 from discord.ext import commands
 
 
@@ -15,10 +16,27 @@ class Utility:
 	
 	@commands.command()
 	@commands.guild_only()
-	async def joined(self, ctx, member: discord.Member):
+	async def whois(self, ctx, member: discord.Member):
 		"""Says when a user joined"""
-		embed = discord.Embed(colour=discord.Colour(0x9013fe), description='{0.mention} joined in {0.joined_at}'.format(member))
-		embed.set_author(name=member.name, icon_url=member.avatar_url)
+		id = member.id
+		av = member.avatar_url
+		nick = member.nick
+		status = member.status
+		activity = member.activity
+		joined = '{0.month}/{0.day}/{0.year} - {0.hour}:{0.minute}'.format(member.joined_at)
+		created = '{0.month}/{0.day}/{0.year} - {0.hour}:{0.minute}'.format(member.created_at)
+		
+		
+		embed = discord.Embed(colour=discord.Colour(0x9013fe))
+		embed.set_author(name=member, icon_url=member.avatar_url)
+		embed.set_image(url=av)
+		embed.add_field(name="Identification",value=id,inline=True)
+		embed.add_field(name="Nickname",value=nick,inline=True)
+		embed.add_field(name="Status",value=status,inline=True)
+		embed.add_field(name="Activity",value=activity,inline=True)
+		embed.add_field(name="Joined At",value=joined,inline=True)
+		embed.add_field(name='Created At',value=created,inline=True)
+		embed.add_field(name='Image',value=av,inline=True)
 		await ctx.send(embed=embed)
 	
 	@commands.command()
