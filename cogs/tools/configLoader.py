d6dce8445fd7569f16d1113a3b4df4e6c2b86d93
Riley Winkler <rex8112@gmail.com>
2018-08-07 05:48:50 -0500

    configLoader makes config.ini when missing

--- cogs/tools/configLoader.py
+++ cogs/tools/configLoader.py
@@ -1,7 +1,19 @@
+import logging
+
 from configobj import ConfigObj
 
-
+logger = logging.getLogger('configLoader')
 config = ConfigObj(infile = 'config.ini', write_empty_values = True)
+
+try:
+	f = open('config.ini')
+	f.close()
+except IOError as e:
+	logger.critical('config.ini not found, token missing.')
+	config['token'] = ''
+	config['logid'] = ''
+	config['owner'] = '180067685986467840'
+	config.write()
 
 class settings:
 	token = config['token']
