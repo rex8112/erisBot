7ca7cecf55afa60d3d0b1f7b25d18a3de77cdca3
Riley Winkler <rex8112@gmail.com>
2018-08-07 03:32:36 -0500

    Added guild owner check in hierarchy()
    Now if you're guild owner, you can succesfully invoke commands no matter the role hierarchy

--- cogs/tools/checks.py
+++ cogs/tools/checks.py
@@ -5,6 +5,8 @@ def hierarchy(ctx, mem: discord.Member):
 	invoker = ctx.author
 	clen = len(invoker.roles) - 1
 	mlen = len(mem.roles) - 1
+	if invoker == ctx.guild.owner:
+		return True
 	if invoker.roles[clen] > mem.roles[mlen]:
 		return True
 	else:
