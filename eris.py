9fc1c05bd6a5a2ad3007c2fc64c7cf70c4137858
Riley Winkler <rex8112@gmail.com>
2019-01-08 11:39:53 -0600

    Disable XP degrade

--- eris.py
+++ eris.py
@@ -12,7 +12,7 @@ from cogs.tools.database import database as db
 startup_extensions = ['cogs.admin', 'cogs.utility', 'cogs.xp', 'cogs.events', 'cogs.meme']
 
 game = discord.Activity(name='.help', type=discord.ActivityType.listening)
-bot = commands.Bot(description='Created by rex8112', command_prefix='.', owner_id=int(settings.owner), activity=game)
+bot = commands.Bot(description='Created by rex8112', command_prefix='t.', owner_id=int(settings.owner), activity=game)
 
 game = discord.Activity(name='.help', type=discord.ActivityType.listening)
 
@@ -32,42 +32,42 @@ async def on_ready():
     bg_task = bot.loop.create_task(degrade())
     logger.info('----- Bot Startup Complete -----')
 
-async def degrade():
-    while True:
-        now = datetime.datetime.now()
-        then = (now + datetime.timedelta(days=1)).replace(hour = 0, minute = 0, second = 0)
-        delta = then - now
-        secs = delta.seconds
-        logger.info('Waiting {} seconds for degrade'.format(secs))
-        await asyncio.sleep(secs)
-        
-        users = db.getAllUsers()
-        for user in users: #Iterates through all users
-            lvl = user[4]
-            xp = user[3]
-            id = user[2]
-            usr = bot.get_user(id)
-            amt = 10 + (lvl * 1)
-            
-            try:
-                if lvl <= 0 and xp - amt < 0: #If running out of XP with no levels to sell
-                    db.updateXP(usr, 0)
-                elif xp - amt < 0: #If running out of XP with levels to sell
-                    lvl = lvl - 1
-                    sell = 300 + (lvl * 100)
-                    remain = amt - xp
-                    xp = sell - remain
-                    db.updateLVL(usr, lvl)
-                    db.updateXP(usr, xp)
-                    
-                    embed = discord.Embed(title = 'Level Lost', colour = discord.Colour(0xd0021b), description = 'Due to low-activity you have lost a level and are now Level **{}**'.format(lvl))
-                    await user.send(embed=embed)
-                    logger.info('Removing Level: {}'.format(usr))
-                else: #Only remaining option is there is XP left to take
-                    db.remXP(usr, amt)
-                    logger.debug('Removing {} XP from {}'.format(amt, usr))
-            except AttributeError:
-                logger.info('{} Not Found For XP Degrade'.format(id))
+##async def degrade():
+##    while True:
+##        now = datetime.datetime.now()
+##        then = (now + datetime.timedelta(days=1)).replace(hour = 0, minute = 0, second = 0)
+##        delta = then - now
+##        secs = delta.seconds
+##        logger.info('Waiting {} seconds for degrade'.format(secs))
+##        await asyncio.sleep(secs)
+##        
+##        users = db.getAllUsers()
+##        for user in users: #Iterates through all users
+##            lvl = user[4]
+##            xp = user[3]
+##            id = user[2]
+##            usr = bot.get_user(id)
+##            amt = 10 + (lvl * 1)
+##            
+##            try:
+##                if lvl <= 0 and xp - amt < 0: #If running out of XP with no levels to sell
+##                    db.updateXP(usr, 0)
+##                elif xp - amt < 0: #If running out of XP with levels to sell
+##                    lvl = lvl - 1
+##                    sell = 300 + (lvl * 100)
+##                    remain = amt - xp
+##                    xp = sell - remain
+##                    db.updateLVL(usr, lvl)
+##                    db.updateXP(usr, xp)
+##                    
+##                    embed = discord.Embed(title = 'Level Lost', colour = discord.Colour(0xd0021b), description = 'Due to low-activity you have lost a level and are now Level **{}**'.format(lvl))
+##                    await user.send(embed=embed)
+##                    logger.info('Removing Level: {}'.format(usr))
+##                else: #Only remaining option is there is XP left to take
+##                    db.remXP(usr, amt)
+##                    logger.debug('Removing {} XP from {}'.format(amt, usr))
+##            except AttributeError:
+##                logger.info('{} Not Found For XP Degrade'.format(id))
         
     
     
